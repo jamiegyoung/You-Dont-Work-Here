e19aa2cf899d9be648ce7e7b5e61d980ad4eb2bd
Jamie Young <jamiegyoung22@gmail.com>
2023-03-22 16:44:04 +0000

    Fix and update old scripts
    - Change attack to interact
    - Remove code related to jumping

--- You Don't Work Here/Assets/Scripts/InputHandler.cs
+++ You Don't Work Here/Assets/Scripts/InputHandler.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 public enum InputHandlerActions
 {
     Move,
-    Attack,
+    Interact,
     Pause
 }
 
--- You Don't Work Here/Assets/Scripts/PlayerMovement.cs
+++ You Don't Work Here/Assets/Scripts/PlayerMovement.cs
@@ -6,31 +6,22 @@ public class PlayerMovement : MonoBehaviour
     public float maxPlayerSpeed;
     public float timeToMaxVelocity;
     public float playerJumpHeight;
-    public LayerMask jumpableGround;
-    public float playerGravity;
 
-    private BoxCollider2D coll;
     private Rigidbody2D rb;
     private Animator anim;
     private SpriteRenderer sprite;
     private InputHandler inputHandler;
     private float currentXVelocity = 0f;
     private Vector2 inputVector = Vector2.zero;
-    private float lastJumpTime = float.NegativeInfinity;
     private float timeSinceLastFootstep;
     private bool leftFoot = false;
     public AudioSource leftFootAudio;
     public AudioSource rightFootAudio;
-    private PlayerHealth playerHealth;
-    public AudioSource jumpAudio;
-    public AudioSource landAudio;
 
     private enum AnimationState
     {
-        idle,
-        running,
-        jumping,
-        falling
+        Idle,
+        Running,
     }
 
     private AnimationState state;
@@ -42,40 +33,24 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
-        coll = GetComponent<BoxCollider2D>();
-        playerHealth = GetComponent<PlayerHealth>();
     }
 
     void Update()
     {
-        if (playerHealth.isDead) return;
         inputVector = inputHandler.GetActionValue<Vector2>(InputHandlerActions.Move);
         UpdateAnimationState();
     }
 
     private void FixedUpdate()
     {
-        if (playerHealth.isDead) return;
-
-        if (inputVector.y > .1f && IsGrounded())
-        {
-            lastJumpTime = Time.time;
-            jumpAudio.Play();
-            rb.velocity = new Vector2(rb.velocity.x, playerJumpHeight);
-        }
-        else if (inputVector.y <= 0f && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, 0f);
-        }
-
         float smoothXVelocity = Mathf.SmoothDamp(rb.velocity.x, inputVector.x * maxPlayerSpeed, ref currentXVelocity, timeToMaxVelocity);
-        rb.velocity = new Vector2(inputVector.x * maxPlayerSpeed, rb.velocity.y);
+        //rb.velocity = new Vector2(inputVector.x * maxPlayerSpeed, rb.velocity.y);
         rb.velocity = new Vector2(smoothXVelocity, rb.velocity.y);
     }
 
     private void PlayWalkAudio()
     {
-        if (timeSinceLastFootstep - Time.time > -0.3f || !IsGrounded()) { return; }
+        if (timeSinceLastFootstep - Time.time > -0.3f) { return; }
         timeSinceLastFootstep = Time.time;
         if (leftFoot)
         {
@@ -96,38 +71,20 @@ public class PlayerMovement : MonoBehaviour
         if (inputVector.x > 0.1f)
         {
             PlayWalkAudio();
-            state = AnimationState.running;
+            state = AnimationState.Running;
             sprite.flipX = false;
         }
         else if (inputVector.x < -0.1f)
         {
             PlayWalkAudio();
-            state = AnimationState.running;
+            state = AnimationState.Running;
             sprite.flipX = true;
         }
         else
         {
-            state = AnimationState.idle;
+            state = AnimationState.Idle;
         }
 
-        if (rb.velocity.y < -.1f)
-        {
-            state = AnimationState.falling;
-            if (IsGrounded())
-            {
-                landAudio.Play();
-            }
-        }
-        else if (rb.velocity.y > .1f)
-        {
-            state = AnimationState.jumping;
-        }
         anim.SetInteger("state", (int)state);
-    }
-
-    private bool IsGrounded()
-    {
-        if (lastJumpTime - Time.time > -.5f) { return false; }
-        return Physics2D.BoxCast(coll.bounds.center, Vector3.Scale(coll.bounds.size, new Vector3(0.9f, 1f, 0.9f)), 0f, Vector2.down, .1f, jumpableGround);
     }
 }
