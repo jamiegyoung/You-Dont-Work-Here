508a3edcd51fea8145f7719e1475400e7d4e93b9
Jamie Young <jamiegyoung22@gmail.com>
2023-03-25 01:56:24 +0000

    Add random hair colours
    Co-Authored-By: JCR101 <71330765+JCR101@users.noreply.github.com>

--- You Don't Work Here/Assets/Scripts/Laptop.cs
+++ You Don't Work Here/Assets/Scripts/Laptop.cs
@@ -27,10 +27,24 @@ public class Laptop : MonoBehaviour, Interactable
             newTemplate.face.sprite = newEmployee.face.faceSprite;
             newTemplate.face.color = Color.Lerp(Color.white, new Color32(0x63, 0x4F, 0x3F, 0xFF), (float)Random.Range(0, 10) / 10);
             newTemplate.hair.sprite = newEmployee.hairSprite;
+
+            // 1 in 10 chance of being gray
+            if (Random.Range(0, 10) == 0)
+            {
+                newTemplate.hair.color = new Color32(0xCD, 0xCD, 0xCD, 0xFF);
+            }
+            else
+            {
+                newTemplate.hair.color = Color.Lerp(
+                    new Color32(0xFF, 0xD4, 0x47, 0xFF),
+                    new Color32(0x1B, 0x19, 0x17, 0xFF),
+                    (float)Random.Range(0, 10) / 10);
+            }
             if (newEmployee.wearsGlasses)
             {
                 newTemplate.glasses.sprite = newEmployee.face.glassesSprite;
-            } else
+            }
+            else
             {
                 // Make the glasses transparent
                 newTemplate.glasses.color = new Color32(0, 0, 0, 0);
