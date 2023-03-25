045e2009585590a951b5f4a5fb4e8e516de626e9
Jamie Young <jamiegyoung22@gmail.com>
2023-03-25 00:19:01 +0000

    Fix laptop height calculations
    - Add laptopheightcalculator script
    - Change height calculations from update to start
    
    Co-Authored-By: JCR101 <71330765+JCR101@users.noreply.github.com>

--- You Don't Work Here/Assets/Scripts/EmployeesPanel.cs
+++ You Don't Work Here/Assets/Scripts/EmployeesPanel.cs
@@ -10,15 +10,8 @@ public class EmployeesPanel : MonoBehaviour
     void Start()
     {
         rectTransform = GetComponent<RectTransform>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("child count " + transform.childCount);
-        Debug.Log(transform.childCount / elementsPerRow);
+        //Debug.Log("child count " + transform.childCount);
+        //Debug.Log(transform.childCount / elementsPerRow);
         rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, 120 * ((transform.childCount / 4) + 1));
-        //Debug.Log(rectTransform.sizeDelta)
-        //gridLayout.
     }
 }
