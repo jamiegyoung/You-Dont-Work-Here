7ef08d18c4d38096e9ffe98c1186591b44ad4d4a
Jamie Young <jamiegyoung22@gmail.com>
2023-03-26 23:16:44 +0100

    Fix employee timelines

--- You Don't Work Here/Assets/Scripts/EmployeeHandler.cs
+++ You Don't Work Here/Assets/Scripts/EmployeeHandler.cs
@@ -11,6 +11,7 @@ public class EmployeeHandler : MonoBehaviour
     public GameObject walkInEmployee;
     public GameObject acceptEmployee;
     public GameObject rejectEmployee;
+    public GameObject closeUpEmployee;
     private PlayableDirector walkInEmployeePlayable;
     private PlayableDirector acceptEmployeePlayable;
     private PlayableDirector rejectEmployeePlayable;
@@ -23,6 +24,7 @@ public class EmployeeHandler : MonoBehaviour
     private EmployeeGenerator employeeGenerator;
     private List<Employee> employeesToProcess;
     private Employee currentEmployee;
+    private Animator closeUpAnim;
 
     void Start()
     {
@@ -34,8 +36,9 @@ public class EmployeeHandler : MonoBehaviour
         acceptEmployee.SetActive(false);
         rejectEmployee.SetActive(false);
         employeesToProcess = employeeGenerator.employees;
+        closeUpAnim = closeUpEmployee.GetComponent<Animator>();
+        closeUpEmployee.SetActive(false);
         StartCoroutine(ProcessEmployees());
-
     }
 
     private IEnumerator ProcessEmployees()
@@ -44,23 +47,29 @@ public class EmployeeHandler : MonoBehaviour
         {
             Debug.Log(employeesToProcess.Count);
             currentEmployee = employeesToProcess[UnityEngine.Random.Range(0, employeesToProcess.Count)];
-            yield return PlayTimelineRoutine(walkInEmployee, walkInEmployeePlayable, PlayFaceAnimation());
+            yield return PlayTimelineRoutine(walkInEmployee, walkInEmployeePlayable, PlayFaceAnimation(), 1);
             employeesToProcess.Remove(currentEmployee);
         }
 
     }
 
+    private IEnumerator WaitForUserProcessing()
+    {
+        // https://answers.unity.com/questions/586609/waiting-for-input-via-coroutine.html
+        closeUpEmployee.SetActive(true);
+        yield return new WaitForSeconds(4);
+        closeUpAnim.SetTrigger("accept");
+    }
+
     private IEnumerator PlayFaceAnimation()
     {
-        //pd.playableAsset = acceptTimeline;
-        //PlayTimelineRoutine(pd);
-        //Debug.Log("hiiii");
+        yield return StartCoroutine(WaitForUserProcessing());
         acceptEmployee.SetActive(true);
         walkInEmployee.SetActive(false);
         yield return StartCoroutine(PlayTimelineRoutine(acceptEmployee, acceptEmployeePlayable));
     }
 
-    private IEnumerator PlayTimelineRoutine(GameObject obj, PlayableDirector playable, float timelineEndingOffset, IEnumerator onComplete)
+    private IEnumerator PlayTimelineRoutine(GameObject obj, PlayableDirector playable, IEnumerator onComplete, float timelineEndingOffset)
     {
         obj.SetActive(true);
         playable.Play();
@@ -73,12 +82,12 @@ public class EmployeeHandler : MonoBehaviour
 
     private IEnumerator PlayTimelineRoutine(GameObject obj, PlayableDirector playable, IEnumerator onComplete)
     {
-        yield return PlayTimelineRoutine(obj, playable, 0, onComplete);
+        yield return PlayTimelineRoutine(obj, playable, onComplete, 0);
     }
 
     private IEnumerator PlayTimelineRoutine(GameObject obj, PlayableDirector playable)
     {
-        yield return PlayTimelineRoutine(obj, playable, 0, null);
+        yield return PlayTimelineRoutine(obj, playable, null, 0);
     }
 
 }
