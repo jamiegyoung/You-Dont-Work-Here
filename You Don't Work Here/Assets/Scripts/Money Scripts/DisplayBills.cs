ddb6377390308ff0e503ad6fe66fd4b147538c34
Jamie Young <jamiegyoung22@gmail.com>
2023-03-30 00:48:06 +0100

    Add fake employees, fix daytracker

--- You Don't Work Here/Assets/Scripts/Money Scripts/DisplayBills.cs
+++ You Don't Work Here/Assets/Scripts/Money Scripts/DisplayBills.cs
@@ -20,13 +20,18 @@ public class DisplayBills : MonoBehaviour
     private float electricity;
     private float food;
     private float total;
-
+    private bool payGas;
+    private bool payFood;
+    private bool payElectricity;
 
     GameObject checkBoxes;
     GameObject billText;
     // Start is called before the first frame update
     void Start()
     {
+        payFood = false;
+        payGas = false;
+        payElectricity = false;
         checkBoxes = GameObject.FindWithTag("CheckBoxes");
         billText = GameObject.FindWithTag("BillText");
         checkBoxes.SetActive(false);
@@ -38,13 +43,13 @@ public class DisplayBills : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(display)
+        if (display)
         {
             // Get which bills the user has selected to pay
-            bool payGas = checkBoxObjects[0].GetComponent<CheckBox>().clicked;
-            bool payElectricity = checkBoxObjects[1].GetComponent<CheckBox>().clicked;
-            bool payFood = checkBoxObjects[2].GetComponent<CheckBox>().clicked;
-            
+            payGas = checkBoxObjects[0].GetComponent<CheckBox>().clicked;
+            payElectricity = checkBoxObjects[1].GetComponent<CheckBox>().clicked;
+            payFood = checkBoxObjects[2].GetComponent<CheckBox>().clicked;
+
             //Calculate current subtotal for the bills selected to be paid
             float subtotal = 0;
             if (payGas) subtotal += gas;
@@ -53,27 +58,27 @@ public class DisplayBills : MonoBehaviour
             float balance = player.GetBalance() - subtotal;
 
             //Update user interface with new totals
-            billText.GetComponent<TextMeshProUGUI>().text = "Check box to pay bill\n\n\nGas: £" + gas + ".00\n\nElectricity: £" + electricity 
-                + ".00\n\nFood: £" + food + ".00\n\n\nTotal: £" + total + ".00\nCurrent Payment: £"+subtotal+".00\nCurrent Balance: £"+balance+".00";
-            
+            billText.GetComponent<TextMeshProUGUI>().text = "Check box to pay bill\n\n\nGas: £" + gas + ".00\n\nElectricity: £" + electricity
+                + ".00\n\nFood: £" + food + ".00\n\n\nTotal: £" + total + ".00\nCurrent Payment: £" + subtotal + ".00\nCurrent Balance: £" + balance + ".00";
+
             //If the users balance after the bills selected are withdrawn then display pay button
-            if(balance > 0)
+            if (balance > 0)
             {
-                payButton.SetActive (true);
+                payButton.SetActive(true);
             }
             else
             {
-                payButton.SetActive (false);
-            }
-
-            // If pay button is clicked, pay the selected bills and update the bill totals
-            if (payButton.GetComponent<CheckBox>().clicked)
-            {
-                bps.PayBills(new Bills(payGas, payFood, payElectricity));
-                UpdateBills();
-                sceneLoader.LoadLevel(SceneLoader.Level.House);
+                payButton.SetActive(false);
             }
         }
+    }
+
+    public void PayBill()
+    {
+        bps.PayBills(new Bills(payGas, payFood, payElectricity));
+        UpdateBills();
+        DayTracker.instance.IncrementDay();
+        sceneLoader.LoadLevel(SceneLoader.Level.House);
     }
     /// <summary>
     /// Method <c>DisplayBill</c> displays the current bill payment screen
