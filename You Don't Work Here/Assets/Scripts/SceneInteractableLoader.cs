193a2f91a25aba847ba388fd9bd1f5aed691f4f2
JakeP2002 <jake0509002v2@hotmail.com>
2023-04-02 22:30:29 +0100

    Added door SFX, glass sliding door audio is cut off slightly by new scene loading in (i assume)

--- You Don't Work Here/Assets/Scripts/SceneInteractableLoader.cs
+++ You Don't Work Here/Assets/Scripts/SceneInteractableLoader.cs
@@ -7,9 +7,18 @@ public class SceneInteractableLoader : MonoBehaviour, Interactable
     public SceneLoader sceneLoader;
     public SceneLoader.Level level;
     public bool IsInteractable => true;
+    private AudioSource sfx;
 
     public void Interact(GameObject interactor)
     {
+        if (level.ToString()== "Walk"){
+            sfx = GetComponent<AudioSource>();
+            sfx.Play(0);
+        }
+        if (level.ToString()== "Reception"){
+            sfx = GetComponent<AudioSource>();
+            sfx.Play(0);
+        }
         sceneLoader.LoadLevel(level);
     }
 
