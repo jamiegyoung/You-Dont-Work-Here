a8fb37543321fa0a8d143931e05183bb5f16bbbe
sdv1 <sdv1@hw.ac.uk>
2023-04-03 03:32:22 +0100

    Completed shopping functionality and added ticks

---
+++ You Don't Work Here/Assets/Scripts/Money Scripts/ProductsLeft.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Money Objects/Products")]
+public class ProductsLeft : ScriptableObject
+{
+    public int flowersCount = 20;
+    public int wineCount = 15;
+    public int headphonesCount = 10;
+    public int watchCount = 5;
+
+    public bool boughtFlowers = false;
+    public bool boughtHeadphones = false;
+    public bool boughtWatch = false;
+    public bool boughtWine = false;
+
+    public enum Products
+    {
+        Flowers,
+        Wine,
+        Headphones,
+        Watch
+    }
+    private void OnEnable()
+    {
+        hideFlags = HideFlags.DontUnloadUnusedAsset;
+    }
+
+    public void decrementSupply()
+    {
+        if (DayTracker.instance.currentDay >1)
+        {
+            flowersCount -= 4;
+            wineCount -= 3;
+            headphonesCount -= 2;
+            watchCount--;
+            if (DayTracker.instance.currentDay % 2 == 0)
+            {
+                watchCount--;
+                flowersCount--;
+            }
+        }
+    }
+
+    public bool buyProduct(Products product)
+    {
+        if (product == Products.Flowers)
+        {
+            if (flowersCount > 0)
+            {
+                boughtFlowers = true;
+                flowersCount--;
+                return true;
+            }
+        }
+        else if (product == Products.Wine)
+        {
+            if (wineCount > 0)
+            {
+                boughtWine = true;
+                wineCount--;
+                return true;
+            }
+        }
+        else if (product == Products.Headphones)
+        {
+            if (headphonesCount > 0)
+            {
+                boughtHeadphones = true;
+                headphonesCount--;
+                return true;
+            }
+        }
+        else if (product == Products.Watch)
+        {
+            if (watchCount > 0)
+            {
+                boughtWatch = true;
+                watchCount--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int GetCount(Products product)
+    {
+        if (product == Products.Flowers)
+        {
+            return flowersCount;
+        }
+        else if (product == Products.Wine)
+        {
+            return wineCount;
+        }
+        else if (product == Products.Headphones)
+        {
+            return headphonesCount;
+        }
+        else
+        {
+            return watchCount;
+        }
+    }
+
+    public bool CheckBought(Products product)
+    {
+        if (product == Products.Flowers)
+        {
+            return boughtFlowers;
+        }
+        else if (product == Products.Wine)
+        {
+            return boughtWine;
+        }
+        else if (product == Products.Headphones)
+        {
+            return boughtHeadphones;
+        }
+        else
+        {
+            return boughtWatch;
+        }
+    }
+
+    public void restart()
+    {
+        flowersCount = 20;
+        wineCount = 15;
+        headphonesCount = 10;
+        watchCount = 5;
+    }
+
+    
+}
---
+++ You Don't Work Here/Assets/Shop.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] itemsSelected;
+
+    [SerializeField] private GameObject[] productTicks;
+
+    [SerializeField] private TextMeshProUGUI[] remainingItems;
+
+    [SerializeField] private TextMeshProUGUI balance;
+
+    [SerializeField]
+    private PlayerMoney player; //Players bank account
+
+    [SerializeField] private GameObject payButton;
+
+    [SerializeField] private ProductsLeft products;
+
+    private float[] prices = new float[4] { 10f, 16f, 30f, 75f };
+
+    private float subtotal = 0;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < itemsSelected.Length; i++)
+        {
+            int count = products.GetCount((ProductsLeft.Products)i) ;
+            if (count < 0) count = 0;
+            bool bought = products.CheckBought((ProductsLeft.Products)i);
+            if(count < 1 || bought)
+            {
+                itemsSelected[i].SetActive(false);
+                productTicks[i].SetActive(true);
+                remainingItems[i].text = count+" remaining";
+            }
+            else
+            {
+                productTicks[i].SetActive(false);
+                itemsSelected[i].SetActive(true);
+                remainingItems[i].text = count + " remaining";
+            }
+        }
+
+        subtotal = 0;
+            for (int i = 0; i < itemsSelected.Length; i++)
+            {
+                if (itemsSelected[i].GetComponent<CheckBox>().clicked && itemsSelected[i].activeSelf)
+                {
+                    subtotal += prices[i];
+                }
+            }
+
+            balance.text = "Subtotal: £" + subtotal.ToString("0.00") + "\n\nBank Balance: £" + (player.GetBalance() - subtotal).ToString("0.00");
+
+        if((player.GetBalance() - subtotal) < 0)
+        {
+            payButton.SetActive(false);
+        }
+        else
+        {
+            payButton.SetActive(true);
+        }
+
+        if (payButton.GetComponent<CheckBox>().clicked)
+        {
+            
+            if (player.Withdraw(subtotal))
+            {
+                for (int i = 0; i < itemsSelected.Length; i++)
+                {
+                    if (itemsSelected[i].GetComponent<CheckBox>().clicked && itemsSelected[i].activeSelf)
+                    {
+                        products.buyProduct((ProductsLeft.Products)i);
+                    }
+                }
+               
+            }
+        }
+       
+    }
+
+  
+
+}
