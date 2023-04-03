016c1a13268e1f4fb69a25ca6a148b0bafa38a9c
HarshMulani <harshmulani@hotmail.com>
2023-04-03 18:45:32 +0100

    Fixed and added winscreen functionality.

--- You Don't Work Here/Assets/Scripts/WinScreenCanvas.cs
+++ You Don't Work Here/Assets/Scripts/WinScreenCanvas.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class EndScreenCanvas1 : MonoBehaviour
+public class WinScreenCanvas : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public SceneLoader sceneLoader;
+    [SerializeField] private TextMeshProUGUI BrotherHappinessText, FatherHappinessText, MotherHappinessText;
+    [SerializeField] private ProductsLeft products;
+
+
     void Start()
     {
-        
+        if (products.boughtFlowers && products.boughtWine)
+        {
+            MotherHappinessText.text = "YOUR MOTHER IS VERY PLEASED WITH YOU,";
+        }
+        else if ((!products.boughtFlowers && products.boughtWine) || (products.boughtFlowers && !products.boughtWine))
+        {
+            MotherHappinessText.text = "YOUR MOTHER IS ALRIGHT WITH YOUR EXISTENCE,";
+        }
+        else
+        {
+            MotherHappinessText.text = "YOUR MOTHER STILL HATES YOU,";
+        }
+
+        if (products.boughtWatch)
+        {
+            FatherHappinessText.text = "YOUR FATHER IS VERY PLEASED WITH YOU,";
+        }
+        else
+        {
+            FatherHappinessText.text = "YOUR FATHER STILL HATES YOU,";
+        }
+
+        if (products.boughtHeadphones)
+        {
+            BrotherHappinessText.text = "AND YOUR BROTHER IS VERY PLEASED WITH YOU";
+        }
+        else
+        {
+            BrotherHappinessText.text = "AND YOUR BROTHER STILL HATES YOU";
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void Restart()
     {
-        
+        Destroy(EmployeeGenerator.instance);
+        EmployeeGenerator.instance = null;
+        Destroy(DayTracker.instance);
+        DayTracker.instance = null;
+        sceneLoader.LoadLevel(SceneLoader.Level.MainMenu);
     }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
 }
